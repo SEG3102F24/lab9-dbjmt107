e610694bffb18086ebf0d4583e61a1cd8649a7d7
Darby <dbjmt107@gmail.com>
2024-11-17 18:50:58 -0500

    first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion = JavaLanguageVersion.of(17)
 	}
 }
 
@@ -25,7 +25,12 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.thymeleaf:thymeleaf:2.0.5")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
 }
 
 kotlin {
